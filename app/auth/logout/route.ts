5dc68501c79587620c4bdd246806a6e1fd39d244
Jonathan Babelotzky <you@example.com>
2025-12-27 19:22:25 +0100

    add(auth): route handler for log out

---
+++ app/auth/logout/route.ts
@@ -0,0 +1,11 @@
+// app/auth/logout/route.ts
+import { NextResponse } from 'next/server'
+import { createClient } from '@/lib/supabase/server'
+
+export async function GET(request: Request) {
+  const supabase = await createClient()
+  await supabase.auth.signOut()
+
+  const url = new URL('/auth/login', request.url)
+  return NextResponse.redirect(url)
+}
